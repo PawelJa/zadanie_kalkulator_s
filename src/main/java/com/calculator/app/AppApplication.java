ce20a6880a9b68e24a9f237098d987dac7b4d5ba
PawelJa <janecki.paw@gmail.com>
2018-04-25 20:52:19 +0200

    main class

--- src/main/java/com/calculator/app/AppApplication.java
+++ src/main/java/com/calculator/app/AppApplication.java
@@ -1,4 +1,4 @@
-package com.calculator.app.config;
+package com.calculator.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
