afeac64ea7b0cdbfae43c0d95bd9e7a11769e904
PawelJa <janecki.paw@gmail.com>
2018-04-26 12:31:05 +0200

    srvice to earning money

--- src/main/java/com/calculator/app/service/CountryService.java
+++ src/main/java/com/calculator/app/service/CountryService.java
@@ -1,4 +1,17 @@
 package com.calculator.app.service;
 
+import com.calculator.app.dao.CountryDAO;
+import com.calculator.app.entity.Country;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CountryService {
+
+    private CountryDAO countryDAO = new CountryDAO();
+
+    public List<Country> getCountriesList() {
+        return countryDAO.getList();
+    }
 }
--- src/main/java/com/calculator/app/service/EarningService.java
+++ src/main/java/com/calculator/app/service/EarningService.java
@@ -1,4 +1,30 @@
 package com.calculator.app.service;
 
+import com.calculator.app.dao.CurrencyDAO;
+import com.calculator.app.entity.Country;
+import com.calculator.app.entity.Currency;
+import com.calculator.app.entity.Earning;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
 public class EarningService {
+
+    public Earning getEarnedMoney(Country country, double earningPerDay) throws IOException {
+        int daysCounter = 22;
+        Earning earning = new Earning();
+        CurrencyDAO currencyDAO = new CurrencyDAO(country.getCurrency());
+        Currency currency = currencyDAO.getRate(country.getCurrency());
+        System.out.println(country.toString());
+
+        earning.setCurency(country.getCurrency());
+        earning.setDate(currency.getEffectiveDate());
+        System.out.println(daysCounter*earningPerDay);
+        System.out.println(1+ (country.getTaxPercent()/100));
+        System.out.println(country.getOncost());
+        earning.setValue( (((daysCounter*earningPerDay) / (1 + (country.getTaxPercent()/100))) - country.getOncost()) * currency.getBid() );
+
+        return earning;
+    }
 }
