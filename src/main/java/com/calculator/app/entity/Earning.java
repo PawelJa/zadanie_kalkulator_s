e014993f923f1e9463de8beb70bc5d59e9fc46d4
PawelJa <janecki.paw@gmail.com>
2018-04-26 12:30:17 +0200

    earning entity

--- src/main/java/com/calculator/app/entity/Earning.java
+++ src/main/java/com/calculator/app/entity/Earning.java
@@ -1,4 +1,49 @@
 package com.calculator.app.entity;
 
 public class Earning {
+    private String date;
+    private String curency;
+    private Double value;
+
+    public Earning() {
+    }
+
+    public Earning(String date, String curency, Double value) {
+        this.date = date;
+        this.curency = curency;
+        this.value = value;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCurency() {
+        return curency;
+    }
+
+    public void setCurency(String curency) {
+        this.curency = curency;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Earning{" +
+                "date='" + date + '\'' +
+                ", curency='" + curency + '\'' +
+                ", value=" + value +
+                '}';
+    }
 }
