b6b244fb403a48ec89646597db0684e428836c62
PawelJa <janecki.paw@gmail.com>
2018-04-26 17:21:29 +0200

    front-end

--- src/main/resources/views/js/app.js
+++ src/main/resources/views/js/app.js
@@ -1,21 +1,89 @@
 API_URL = "http://localhost:8080/";
 
 $(document).ready(function () {
+
+    $('button').on('click', function (e) {
+        getEarnedMoney(e);
+    })
+
     getCountries();
+
 
     function getCountries() {
         $.ajax({
             url: API_URL + "country"
         }).done(function (countries) {
-            console.log(countries);
-            showCountries(countries);
+            // console.log(countries)
+            showCountries(countries)
         })
     }
 
     function showCountries(countries) {
-        var select = $('input.countries')
+        var select = $('select')
+        // console.log(select);
 
-        countries.forEach()
+        countries.forEach( function (e) {
+            select.append("<option value='" + e.id + "'>" + e.fullName + "</option>");
+        })
+    }
+
+    function getEarnedMoney(e) {
+        e.preventDefault();
+
+        var selectedId = $('select').val();
+        var value = $('#payRate').val();
+
+        var id = null;
+        var fullName = null;
+
+        if ($.isNumeric(value) && value > 0) {
+            $('#error').text('');
+
+            $.ajax({
+                url: API_URL + "country"
+            }).done(function (countries) {
+                // console.log(countries)
+                var result = countries.find(x => x.id === selectedId);
+                // console.log(result)
+
+                var objToSend = {
+                    id : result.id,
+                    fullName : result.fullName,
+                    oncost : result.oncost,
+                    taxPercent : result.taxPercent,
+                    currency : result.currency
+                }
+                var headers = new Headers({
+                    'Content-Type': 'application/json'
+                });
+
+                var myInit = {
+                    method: 'POST',
+                    headers: headers,
+                    cache: 'default',
+                    body: JSON.stringify(objToSend)
+                };
+
+                // $.ajax({
+                //     url: API_URL+'/earnings/' + value, myInit).done(function () {
+                //
+                // })
+                // })
+
+                fetch(API_URL+'/earnings/' + value, myInit).then(resp => resp.json()).then(resp => {
+                    var earnings = $('#earnings')
+                    earnings.empty()
+                    earnings.append('<div class="earningsChild">Your earnings:</div>');
+                    earnings.append('<div class="earningsChild">' + resp.value + '</div>')
+                    earnings.append('<div class="earningsChild">Used rate from: ' + resp.date + '</div>');
+                });
+
+            })
+        } else {
+            $('#error').text("Wrong value :( . It must be numeric and positive ;)");
+        }
+
+
     }
 })
 
